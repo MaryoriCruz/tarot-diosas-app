b316aab0188ab34baab3e560bf4f0ce7e950252a
MaryoriCruz <maryori.eguizabal@gmail.com>
2025-08-22 04:52:21 -0500

    feat:improved project with buttons, texts, footer and responsive working.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2025 Diosas Contemporáneas - Celebrando el poder femenino en STEM</p>
+      <p>Proyecto Educativo - Maryori Cruz</p>
     </footer>
   );
 };
